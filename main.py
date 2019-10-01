dbd633dbf2129ea8849dea59d6ae9ec96e4c9999
toniferra <registro@toniferra.com>
2019-10-02 00:35:05 +0200

    fase lógica funcionando

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # iniciando librerias
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 from horoscopo import HoroscopoChino
 app = Flask(__name__)
 
@@ -9,6 +13,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/signo', methods=['POST'])
+
+@app.route('/', methods=['POST'])
+def logica():
+    # recuperar desde el formulario la variable anyo
+    anyo = int(request.form['anyo'])
+
+    # crear el objeto
+    persona = HoroscopoChino(anyo)
+
+    global signo_usuario
+    # llamar a nuestro método signo
+    signo_usuario = persona.signo()
+
+    return redirect(url_for('signo'))
+
+
+@app.route('/signo')
 def signo():
-    return render_template('signo.html')
+    return render_template('signo.html', signo_usuario=signo_usuario)
+
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug=True)
